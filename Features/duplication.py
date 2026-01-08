22941b1b6964300977341d3cddf4066b828a8ba9
Hrishikesh Dhuri <121174859+Hrishikesh19032004@users.noreply.github.com>
2026-01-08 22:57:54 +0530

    Added prefiltering

--- Features/duplication.py
+++ Features/duplication.py
@@ -1,39 +1,53 @@
 import hashlib
 import os 
 import shutil 
+from collections import defaultdict
 
 def file_hash(path, chunk_size = 8192):
-    hash = hashlib.md5()
+    hashMD5 = hashlib.md5()
     with open(path,'rb') as f:
         while True:
             chunk = f.read(chunk_size)
             if not chunk:
                 break
-            hash.update(chunk)
-        return hash.hexdigest()
+            hashMD5.update(chunk)
+        return hashMD5.hexdigest()
 
 
 def duplication_Handle(dir):
-    seen_hash = set()
+    
     duplicateFolder = os.path.join(dir,"Duplicates")
     os.makedirs(duplicateFolder, exist_ok=True)
 
+    size_map = defaultdict(list)
     for root, dirs, files in os.walk(dir):
         if "Duplicates" in dirs:
             dirs.remove('Duplicates')
 
         for file in files:
             path = os.path.join(root,file)
-            h = file_hash(path)
-            if h in seen_hash:
-                print(f"Moving {file} to duplicate folder. ")
-                base, ext = os.path.splitext(file)
-                new_name = base
-                counter = 1
-                while os.path.exists(os.path.join(duplicateFolder, new_name)):
-                    new_name = f"{base}_{counter}{ext}"
-                    counter+=1
-                shutil.move(path,os.path.join(duplicateFolder,new_name))
-            else:
-                seen_hash.add(h)
+            size = os.path.getsize(path)
+            size_map[size].append(path)
+
+    for size, paths in size_map.items():
+            if len(paths)<2:
+                continue
+            
+            seen_hash = {}
+
+            for path in paths:
+                h = file_hash(path)
+                file_name = os.path.basename(path)
+
+                if h in seen_hash:
+                    print(f"Moving {file_name} to duplicate folder. ")
+                    base, ext = os.path.splitext(file_name)
+                    new_name = file_name
+                    counter = 1
+                    while os.path.exists(os.path.join(duplicateFolder, new_name)):
+                        new_name = f"{base}_{counter}{ext}"
+                        counter+=1
+                    shutil.move(path,os.path.join(duplicateFolder,new_name))
+                else:
+                    seen_hash[h]=path
     
